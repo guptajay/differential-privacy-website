5e6c59da5dce25c669243d787c45f4912b4e3c8e
Ritik Bhatia <bhatia.ritik5@gmail.com>
2020-02-26 00:55:39 +0530

    Added table view

--- __init__.py
+++ __init__.py
@@ -35,7 +35,7 @@ app = Flask(__name__)
 def index():
     query = '''SELECT * FROM census LIMIT 10;'''
     queryResult = pd.read_sql_query(query, psqlconn)
-    return render_template('index.html', dbPreview=queryResult)
+    return render_template('index.html', dbPreview=queryResult.to_html())
 
 
 @app.route('/handle_submit', methods=['POST'])
@@ -43,6 +43,7 @@ def handle_submit():
     # To handle standard query
     stdQuery = request.form['stdQuery']
     stdQueryResult = pd.read_sql_query(stdQuery, psqlconn)
+    
 
     # To handle adversarial query
     advQuery = request.form['advQuery']
@@ -52,26 +53,25 @@ def handle_submit():
     jointData = pd.merge(stdQueryResult, advQueryResult, on='salary')
     jointMelt = pd.melt(jointData, id_vars="salary",
                         var_name="query", value_name="count")
-    fig_std = sb.catplot(x='count', y='salary', hue='query',
-                         data=jointMelt, kind='bar', height=10)
+    sb.catplot(x='count', y='salary', hue='query', data=jointMelt, kind='bar', height=10)
 
-    img_std = StringIO.StringIO()
-    plt.savefig(fig_std, format='png')
+    img_std = StringIO()
+    plt.savefig(img_std, format='png')
     plt.close()
     img_std.seek(0)
     plot_url_std = base64.b64encode(img_std.getvalue())
 
     # Join the two Dataframes on `salary` and check the difference
     jointData["difference"] = jointData["count_x"] - jointData["count_y"]
-    fig_adv = sb.barplot(x="difference", y="salary", data=jointData)
+    sb.barplot(x="difference", y="salary", data=jointData)
 
-    img_adv = StringIO.StringIO()
-    plt.savefig(fig_adv, format='png')
+    img_adv = StringIO()
+    plt.savefig(img_adv, format='png')
     plt.close()
     img_adv.seek(0)
     plot_url_adv = base64.b64encode(img_adv.getvalue())
 
-    return render_template('/plots', plot_url_std=plot_url_std, plot_url_adv=plot_url_adv)
+    return render_template('plots.html', plot_url_std=plot_url_std, plot_url_adv=plot_url_adv)
 
 
 if __name__ == "__main__":
